03ef2b6ea06ba20601c82c674bbd6ff7fb0e7974
ggikko.park <ggikko.park@kakao-bank.com>
2017-10-17 21:41:32 +0900

    init

---
+++ src/main/java/hello/HelloGgikko.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by ggikko on 2017. 10. 17..
+ */
+public class HelloGgikko {
+
+    public String getMessage(boolean isGgikko) {
+        if (isGgikko) {
+            return "Hello Ggikko!";
+        } else {
+            return "Hello World!";
+        }
+    }
+}
